9a0758b317383b0e65e11e9feaf4adcb9342b36a
DavideSberna <sbernadavide@gmail.com>
2023-09-13 17:56:31 +0200

    add new control to input

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -9,18 +9,42 @@ public class PasswordGenerator {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Il tuo nome: ");
+		while (sc.hasNextInt()) {
+            System.out.print("Inserisci un valore alfabetico. Il tuo nome: ");
+            sc.next();
+        }
 		String name = sc.nextLine();
 		
+		
+		
 		System.out.print("Il tuo cognome: ");
+		while (sc.hasNextInt()) {
+            System.out.print("Inserisci un valore alfabetico. Il tuo cognome: ");
+            sc.next();
+        }
 		String surname = sc.nextLine();
 		
+		
+		
 		System.out.print("Il tuo colore: ");
+		while (sc.hasNextInt()) {
+            System.out.print("Inserisci un valore alfabetico. Il tuo colore: ");
+            sc.next();
+        }
 		String color = sc.nextLine();
 		
-		System.out.print("Il tuo età: ");
+		
+		
+		System.out.print("La tua età: ");
+		while (sc.hasNextInt()) {
+            System.out.print("Inserisci un valore alfabetico. La tua età: ");
+            sc.next();
+        }
 		String age = sc.nextLine();
 		
+		
 		String ageFormatting = "";
+		
 		for(int i = 0; i < age.length(); i++) {
 			if(i >= age.length() - 4) {
 				ageFormatting += age.charAt(i);
